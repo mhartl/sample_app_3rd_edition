0a0580594d49cfe9494f6fd18fdd7c3f4edf3575
Michael Hartl <michael@michaelhartl.com>
2014-10-29 02:24:54 +0000

    Fix formatting

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,10 +16,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "Example User",
-                                    email: "user@example.com",
-                                    password:              "password",
-                                    password_confirmation: "password" }
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
   end
