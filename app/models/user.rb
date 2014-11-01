8482ffb1991356f8739cbf97c85e654b5c142ece
Michael Hartl <michael@michaelhartl.com>
2014-11-01 01:32:36 +0000

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
   
-  # Retruns true if the given token matches the digest.
+  # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
