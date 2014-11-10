ba5a178339f661ad26f3fbb176753affaf37c793
Michael Hartl <michael@michaelhartl.com>
2014-11-10 01:08:19 +0000

    Add method & before filters

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,7 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -21,11 +23,7 @@ class PasswordResetsController < ApplicationController
   end
   
   def update
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
-    elsif (params[:user][:password].blank? && 
-           params[:user][:password_confirmation].blank?)
+    if both_passwords_blank?
       flash.now[:danger] = "Password/confirmation can't be blank"
       render 'edit'
     elsif @user.update_attributes(user_params)
@@ -42,12 +40,32 @@ class PasswordResetsController < ApplicationController
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
+    
+    # Returns true if password & confirmation are blank.
+    def both_passwords_blank?
+      params[:user][:password].blank? && 
+      params[:user][:password_confirmation].blank?
+    end
+  
+    # Before filters
   
     def get_user
       @user = User.find_by(email: params[:email])
+    end
+    
+    # Confirms a valid user.
+    def valid_user
       unless (@user && @user.activated? && 
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end      
+    end
+    
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 end
