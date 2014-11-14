feb09e4c878fa70e701ae1333cf511fc549c1e28
Michael Hartl <michael@michaelhartl.com>
2014-11-14 10:38:18 -0800

    Remove explicit Ruby version

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -10,22 +10,22 @@ class MicropostTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @micropost.valid?
   end
-  
+
   test "user id should be present" do
     @micropost.user_id = nil
     assert_not @micropost.valid?
   end
-  
+
   test "content should be present" do
     @micropost.content = "   "
     assert_not @micropost.valid?
   end
-  
+
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
   end
-  
+
   test "order should be most recent first" do
     assert_equal Micropost.first, microposts(:most_recent)
   end
