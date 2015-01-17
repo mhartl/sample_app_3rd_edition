657e2775016b5f5699b67437ef9bc1726ab87241
Michael Hartl <michael@michaelhartl.com>
2015-01-16 18:57:08 -0800

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141030174712) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
