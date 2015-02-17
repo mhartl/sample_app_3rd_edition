b672050393a877f25caf6518673dea85fe226754
Michael Hartl <michael@michaelhartl.com>
2015-02-17 10:04:58 -0800

    Update micropost migration with foreign key contraint

--- db/migrate/20141110194807_create_microposts.rb
+++ db/migrate/20141110194807_create_microposts.rb
@@ -6,6 +6,7 @@ class CreateMicroposts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_foreign_key :microposts, :users
     add_index :microposts, [:user_id, :created_at]
   end
 end
