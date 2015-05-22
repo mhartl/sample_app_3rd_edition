8d4ecce67f18a1b658239944231776e46ad46fe0
Michael Hartl <michael@michaelhartl.com>
2015-05-22 10:07:38 -0700

    Add test for blank passwords and a presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
   def User.digest(string)
