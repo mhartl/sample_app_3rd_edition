f24b5d20f7f1377ec22da2fff3546a61a965ebaf
Michael Hartl <michael@michaelhartl.com>
2015-05-22 10:50:20 -0700

    Add allow_nil: true

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -25,8 +25,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     email = "foo@bar.com"
     patch user_path(@user), user: { name:  name,
                                     email: email,
-                                    password:              "foobar",
-                                    password_confirmation: "foobar" }
+                                    password:              "",
+                                    password_confirmation: "" }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
