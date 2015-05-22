d3c00815ffcf2887fc19cb9ecac09cb7762b6561
Michael Hartl <michael@michaelhartl.com>
2015-05-21 17:55:59 -0700

    Remove unnecessary capture of blank passwords

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,15 +18,12 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    if password_blank?
-      flash.now[:danger] = "Password can't be blank"
-      render 'edit'
-    elsif @user.update_attributes(user_params)
+    if @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "Password has been reset."
       redirect_to @user
@@ -34,32 +31,32 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
-    
+
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
-    
+
     # Returns true if password is blank.
     def password_blank?
       params[:user][:password].blank?
     end
-  
+
     # Before filters
-  
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
-    
+
     # Confirms a valid user.
     def valid_user
-      unless (@user && @user.activated? && 
+      unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
-      end      
+      end
     end
-    
+
     # Checks expiration of reset token.
     def check_expiration
       if @user.password_reset_expired?
