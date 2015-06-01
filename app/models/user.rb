b25c8b77a2c00d19a98dfb5efe89259e69ed0845
Michael Hartl <michael@michaelhartl.com>
2015-05-31 17:26:14 -0700

    Use downcase-bang

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
   has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
-  before_save   :downcase_email
+  before_save   { email.downcase! }
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
