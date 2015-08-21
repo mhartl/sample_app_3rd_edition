205621394de6fd7ec42bb25b3c8abd350ae0e666
Hiroaki Ninomiya <ninomiya@spacemarket.co.jp>
2015-08-21 11:44:44 +0900

    Fix host for mail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'gentle-garden-7691.herokuapp.com'
+  host = ENV['HEROKU_APP_HOST'] || 'gentle-garden-7691.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
