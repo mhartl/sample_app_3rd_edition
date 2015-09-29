9b67bddb9ae9ce2cad89f6278420459b2783f6b0
Michael Hartl <michael@michaelhartl.com>
2015-09-28 20:49:02 -0700

    Go back to previous solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
