37253b2c286f20571921ce33db0581a6ec089e60
hayashi_kengo <hayashikengojp@gmeil.com>
2016-04-21 21:23:03 +0900

    Add ajax (not complete)

--- app/controllers/my_favorites_controller.rb
+++ app/controllers/my_favorites_controller.rb
@@ -2,11 +2,13 @@ class MyFavoritesController < ApplicationController
 	
 	def create
 		@micropost = Micropost.find(params[:micropost_id])
+		@user = current_user
 		current_user.favorite(@micropost)
 	end
 	
 	def destroy
 		@micropost = Micropost.find(params[:micropost_id]).destroy
+		@user = current_user
 		current_user.unfavorite(@micropost)
 	end
 end
