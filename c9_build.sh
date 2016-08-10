30b9151e8be79349a60cbad8cc3027b0f3ca00c7
Mark Underseth <marku@s2technologies.com>
2016-08-10 23:08:52 +0000

    using native rubocop format

--- c9_build.sh
+++ c9_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Analysis
-bundle exec rubocop --require rubocop/formatter/checkstyle_formatter --format RuboCop::Formatter::CheckstyleFormatter --no-color --rails --out tmp/rubocop_checkstyle.xml
+bundle exec rubocop --format emacs --out tmp/rubocop.txt
 bundle exec brakeman -o tmp/brakeman.json
 bundle exec brakeman_translate_checkstyle_format translate --file="tmp/brakeman.json" > tmp/brakeman_checkstyle.xml
 bundle exec scss-lint --require=scss_lint_reporter_checkstyle --format=Checkstyle --out=tmp/scss-lint_checkstyle.xml  app/assets/stylesheets/
@@ -12,4 +12,4 @@ bundle exec rake minitest test
 # Publish
 curl -s https://testspace-client.s3-us-west-2.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
 export TEST_REPORTS=$PWD/test/reports
-testspace publish @.testspace master.c9
+testspace publish @.testspace $C9_BRANCH.c9
