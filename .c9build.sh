7716b6551d7e8600b5c0e690c2ed2ecf7306f838
Mark Underseth <marku@s2technologies.com>
2016-08-23 00:03:49 +0000

    using scss-lint default output

--- .c9build.sh
+++ .c9build.sh
@@ -8,7 +8,7 @@
 bundle exec rubocop --format emacs --out tmp/rubocop.txt
 bundle exec brakeman -o tmp/brakeman.json
 bundle exec brakeman_translate_checkstyle_format translate --file="tmp/brakeman.json" > tmp/brakeman_checkstyle.xml
-bundle exec scss-lint --require=scss_lint_reporter_checkstyle --format=Checkstyle --out=tmp/scss-lint_checkstyle.xml  app/assets/stylesheets/
+bundle exec scss-lint --no-color --format=Stats --format=Default --out=tmp/scss-lint.txt  app/assets/stylesheets/
 
 # Test
 export CI_REPORTS=$PWD/test/reports
@@ -16,5 +16,4 @@ bundle exec rake minitest test
 
 # Publish
 # curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-# export CI_REPORTS=$PWD/test/reports
-# testspace publish @.testspace BRACH-NAME.c9
+# CI_REPORTS=$PWD/test/reports testspace publish @.testspace BRACH-NAME.c9
