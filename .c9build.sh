e5e0f34e50140c8d888c1fd83f8f7731da58d6e2
Mark Underseth <marku@s2technologies.com>
2016-08-30 14:56:25 +0000

    unifying build scripts

--- .c9build.sh
+++ .c9build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build
-# gem install bundler
-# bundle install
+gem install bundler
+bundle install
 
 # Analysis
 bundle exec rubocop --format emacs --out tmp/rubocop.txt
@@ -16,4 +16,4 @@ bundle exec rake minitest test
 
 # Publish
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-CI_REPORTS=$PWD/test/reports testspace publish @.testspace master.c9
+testspace publish @.testspace master.c9
