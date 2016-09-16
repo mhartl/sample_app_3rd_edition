f822a23f0be01bc32440d0b37e089cc7e5aa6d85
Mark Underseth <marku@s2technologies.com>
2016-09-16 21:55:49 +0000

    client 1.4 updates

--- .c9build.sh
+++ .c9build.sh
@@ -16,4 +16,4 @@ bundle exec rake minitest test
 
 # Publish
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-testspace publish @.testspace master.c9
+testspace @.testspace master.c9
