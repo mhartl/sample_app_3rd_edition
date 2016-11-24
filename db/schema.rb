e5f7aac21a00b5883edf524609a13319e55cf33c
Yoshitomo Yasuno <yasunodayo@yahoo.com>
2016-11-24 15:56:17 +0900

    Finished add reply function

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20161124012623) do
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "picture"
-    t.string   "in_reply_to", default: ""
+    t.integer  "in_reply_to"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
