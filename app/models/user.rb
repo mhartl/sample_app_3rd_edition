986096486a7f537479f812a15964a5581207c3d8
Daisuke Ono <newzealand55555@gmail.com>
2017-02-03 11:32:34 +0900

    fix validation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :introduction, length: { maximum: 100 }
+  validates :introduction, length: { maximum: 1000 }
   validates :nationality, length: { maximum: 20 }
 
   # Returns the hash digest of the given string.
