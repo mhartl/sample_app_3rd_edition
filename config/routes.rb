4510e86e890cab9f3fd5a766250f2d4e1f908530
Daisuke Ono <newzealand55555@gmail.com>
2017-02-04 18:33:47 +0900

    add routes to comment

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
+  resources :microposts,          only: [:create, :show, :destroy] do
+    resources :comments,          only: [:create, :destroy]
+  end
   resources :relationships,       only: [:create, :destroy]
 end
