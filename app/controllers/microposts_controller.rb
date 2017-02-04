2f0fd1b592d73a971ec54a64349e52c2989e944e
Daisuke Ono <newzealand55555@gmail.com>
2017-02-04 20:36:48 +0900

    set variables in micro posts controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,12 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
   
+  def show
+    @micropost = Micropost.find(params[:id])
+    @comment = Comment.new
+    @comments = @micropost.comments.includes(:user)
+  end
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
