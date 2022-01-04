d9599d66725dd29e617d3ee0784a2734fb22cde2
ubuntu <ubuntu@localhost.localdomain>
2022-01-04 11:44:30 +0800

    test pr for sg

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
+  get     'hello'  => 'static_pages#hello'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
   get    'signup'  => 'users#new'
